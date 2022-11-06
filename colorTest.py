c2ee3885122052950b9edc07327721baaf7af80b
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2022-11-05 17:19:34 -0700

    small changes

---
+++ colorTest.py
@@ -0,0 +1,97 @@
+import docx
+from docx import Document
+from docx.shared import RGBColor
+from tkinter import *
+from tkinter import filedialog
+
+
+def readtxt(filename, color: tuple[int, int, int]):
+    doc = docx.Document(filename)
+
+    fullText = []
+    for para in doc.paragraphs:
+
+        # Getting the colored words from the doc
+        if (getcoloredTxt(para.runs, color)):
+
+            # Concatenating list of runs between the colored text to single a string
+            sentence = "".join(r.text for r in para.runs)
+            fullText.append(sentence)
+
+    return fullText
+
+def getcoloredTxt(runs, color):
+
+    coloredWords, word = [], ""
+    for run in runs:
+        if run.font.color.rgb == RGBColor(*color):
+            word += str(run.text)
+
+        elif word != "":
+            coloredWords.append(word)
+            word = ""
+    if word != "":
+        coloredWords.append(word + "\n")
+
+    return coloredWords
+
+def openFile():
+    global filepath
+    global filepath2
+    filepath = filedialog.askopenfilename(initialdir="/",
+                                          title="",
+                                          filetypes= (("word documents","*.docx"),
+                                                      ("all files","*.*")))
+    file = open(filepath,'r')
+    #print(filepath)
+    file.close()
+    filepath2 = str(filepath)
+    #filepath2 = '"' + filepath + '"'
+    print(filepath2)
+
+    return filepath2
+
+
+
+
+if __name__ == '__main__':
+    window = Tk()
+    button = Button(text="Open",command=openFile)
+    button.pack()
+    Button(window, text="Quit", command=window.destroy).pack()
+    window.mainloop()
+    #filepath2 = '"' + filepath + '"'
+    #print(filepath2)
+    #fullText = readtxt("testred.docx")
+    #print(fullText)
+    #filepath3 = filepath2
+    #print(filepath3)
+    fullText = readtxt(filename=filepath2,
+                   color=(255, 0, 0))
+    print(fullText)
+    #lister2 = [fullText]
+    #d = ''.join(fullText)
+    #print(fullText)
+    #print(d)
+    #words = d.split('')
+    #words2 = d.split('')
+
+    #print(words)
+
+    #report = Document()
+#paragraph = report.add_paragraph(fullText[1])
+#paragraph = report.add_paragraph(fullText)
+#paragraph = report.add_paragraph(s)
+#report.save('report1.docx')
+
+
+    #w = (fullText.replace (']', ']\n'))
+    #paragraph = report.add_paragraph()
+    #runner = paragraph.add_run("\nSRS Ace Pump Document")
+    #runner.bold = True #makes the header bold
+    #paragraph = report.add_paragraph(w)
+    #report.save('report1.docx')
+
+
+
+
